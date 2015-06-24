92e317e4053444ae091ead7dbcab9d50be3ca590
Brian <bcf.vandenbroek@student.avans.nl>
2015-06-24 11:59:45 +0200

    server 2

--- assets/js/zeeslag.js
+++ assets/js/zeeslag.js
@@ -1,4 +1,4 @@
-var server = /*"https://zeeslagavans.herokuapp.com/"*/"http://zeeslagavans3.herokuapp.com/";
+var server = /*"https://zeeslagavans.herokuapp.com/"*/"http://zeeslagavans2.herokuapp.com/";
 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ImNqZ2hldW1lQGF2YW5zLm5sIg.DNq6H0fKb5NofVyq3kQPdWZLEiPdwFLSmCw0EXDZ_CE";
 
 $(function () {
